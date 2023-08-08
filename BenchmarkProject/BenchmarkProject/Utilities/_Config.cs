8f2cea37061614f41e82eb16cf4bca58e5f94908
arazcengiz <88026241+arazcengiz@users.noreply.github.com>
2023-08-08 15:38:06 +0200

    Add files via upload

---
+++ BenchmarkProject/BenchmarkProject/Utilities/_Config.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Reports;
+
+namespace BenchmarkProject.Utilities
+{
+    internal class _Config : ManualConfig
+    {
+        public _Config()
+        {
+            base.SummaryStyle = SummaryStyle.Default.WithRatioStyle(BenchmarkDotNet.Columns.RatioStyle.Percentage);
+        }
+    }
+}
